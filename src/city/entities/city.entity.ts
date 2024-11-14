955e9466cd8d0675a1dd0170f5b66fb3b7709654
Dan Sentillas <d.sentillas@arcanys.com>
2024-11-14 13:34:40 +0800

    fix declaration of city entity

--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,5 +1,6 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'cities' })
 export class City {
   @PrimaryGeneratedColumn()
   id: number;
